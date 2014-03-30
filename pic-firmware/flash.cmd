44286fd102c98063aa4fbb9618f932b4dfed780d
Frank Buss <fb@frank-buss.de>
2014-03-30 10:11:51 +0200

    PIC flash script for PICkit 2

---
+++ pic-firmware/flash.cmd
@@ -0,0 +1,2 @@
+rem flash script for PICkit 2 and https://code.google.com/p/pic32prog/
+"C:\Program Files\pic32prog-read-only\pic32prog" --skip-verify gandalf.X\dist\default\production\gandalf.X.production.hex
